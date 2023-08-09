64675de6e5334141ca5fb34acc3a50b913e22fd8
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-08-09 14:34:06 +0300

    Create FileStorage class to serialize and deserialize instances

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ This is the basemodel module and it defines the BaseModel class
 """
 from datetime import datetime
 import uuid
-
+from . import storage
 
 class BaseModel():
     """
@@ -28,6 +28,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -42,6 +43,7 @@ class BaseModel():
         with the current date time
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# This module defines the FileStorage class
+import json
+import os
+
+
+class FileStorage():
+    """
+    Serializez instances to a JSON file and also
+    Deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(new):
+        """
+        Returns a dictionary stored in __objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <classname.id>
+        """
+        class_name = obj.__class__.__name__
+        id = obj.id
+        key = f"{class_name}.{id}"
+        FileStorage.__objects[key] = obj
+
+    def import_class(self):
+        """
+        Imports needed class names
+        Returns a dictionary with the needed classes
+        """
+        from models.base_model import BaseModel
+
+        return {'BaseModel': BaseModel}
+
+    def save(self):
+        """
+        Serializes __objects to the json file
+        """
+        json_dictionary = {}
+        for key, obj in FileStorage.__objects.items():
+            json_dictionary[key] = obj.to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(json_dictionary, file)
+
+    def reload(self):
+        """
+        deserializes the json file to __objects
+        """
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+                json_data = json.load(file)
+                for key, json_dict in json_data.items():
+                    class_name, obj_id = key.split('.')
+                    new_instance = self.import_class()[class_name](**json_dict)
+                    FileStorage.__objects[key] = new_instance
