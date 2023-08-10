a714563f7e0ab3e7820f7fdf37227e01a3cc9b0f
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-08-10 19:06:49 +0300

    Add destroy method to destroy an instance

--- console.py
+++ console.py
@@ -53,6 +53,39 @@ class HBNBCommand(cmd.Cmd):
                 if id_exists is False:
                     print("** no instance found ** ")
 
+    def do_destroy(self, args):
+        """
+        Deletes an instance based on class name and id
+        """
+        if args == "":
+            print("** class name is missing **")
+        else:
+            args = args.split()
+            name = args[0]
+            if len(args) == 1:
+                obj_id = ""
+            else:
+                obj_id = args[1]
+
+            if name not in globals():
+                print("** class doesn't exist **")
+            elif obj_id == "":
+                print("** instance id is missing **")
+            else:
+                id_exists = False
+                objects = storage.all()
+
+                for key, obj in objects.items():
+                    if obj_id == obj.id:
+                        id_exists = True
+                        to_delete = key
+
+                if id_exists is False:
+                    print("** no instance found **")
+                else:
+                    del objects[to_delete]
+                    storage.save()
+
     def do_quit(self, arg):
         """
         Quits and exits the program
@@ -73,18 +106,33 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     # custom help commands
+    def help_show(self):
+        """
+        Help command for show method
+        """
+        print("Prints the string representation of an instance")
+        print("Usage: show <class_name> <id>")
+
     def help_create(self):
         """
         Custom help command for create method
         """
         print("Creates an new instance of a class and saves it to JSON file")
-        print("Usage: <create class_name>")
+        print("Usage: create <class_name>")
+
+    def help_destroy(self):
+        """
+        Shows description od destroy command
+        """
+        print("Deletes an instance based on class name and id")
+        print("Usage: destroy <class_name> <id>")
 
     def help_quit(self):
         """
         Custom Help guide for quit
         """
         print("Quits and exits the program")
+        print("Usage: quit")
 
     def help_EOF(self):
         """
